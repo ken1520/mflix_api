6d24f720d834eb046ff70343516a4693434717bb
Ken Lee <ken_lee@mtelnet.com>
2021-04-13 12:43:24 +0800

    Set user registration API structure

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,5 +4,6 @@ const userProcessor = require('../processors/user/userProcessor');
 let router = new Router({ caseSensitive: true });
 
 router.post('/login', userProcessor.userLogin);
+router.post('/register', userProcessor.userRegistration);
 
 module.exports = router;
--- processors/user/userProcessor.js
+++ processors/user/userProcessor.js
@@ -8,19 +8,6 @@ const User = require('../../models/user');
 const jwt = require('../../utils/jwt');
 const apiResponse = require('../../utils/apiResponse');
 
-let getMovieById = async (req, res) => {
-    try {
-        if (validator.isMongoId(req.params.movieId)) {
-            let resultMovie = await Movie.find({ _id: req.params.movieId });
-            apiResponse(req, res, resultMovie.length > 0 ? 1001 : 1002, resultMovie);
-        } else {
-            apiResponse(req, res, 1008, {});
-        }
-    } catch (error) {
-        apiResponse(req, res, 1009, error);
-    }
-};
-
 let userLogin = async (req, res) => {
     // let commonPW = await bcrypt.hash('123456', bcrypt.genSaltSync(parseInt(process.env.SALT_LENGTH)));
     // let updateResult = await User.updateMany({}, { password: commonPW });
@@ -47,7 +34,17 @@ let userLogin = async (req, res) => {
     }
 };
 
+let userRegistration = async (req, res) => {
+    // let commonPW = await bcrypt.hash('123456', bcrypt.genSaltSync(parseInt(process.env.SALT_LENGTH)));
+    // let updateResult = await User.updateMany({}, { password: commonPW });
+    try {
+        
+    } catch (error) {
+        apiResponse(req, res, 1009, error);
+    }
+};
+
 module.exports = {
     userLogin: userLogin,
-    getMovieById: getMovieById
+    userRegistration: userRegistration
 }
