f14ea0bef9e9975193fe453bdb7a7700bf0e1dca
Ken Lee <ken_lee@mtelnet.com>
2021-04-13 16:16:20 +0800

    User login validation

--- config/response.js
+++ config/response.js
@@ -11,6 +11,10 @@ module.exports = {
         systemMessage: 'no this email',
         displayMessage: 'User not found'
     },
+    1006: {
+        systemMessage: 'invalid user input',
+        displayMessage: 'Invalid input - email or password'
+    },
     1007: {
         systemMessage: 'invalid email',
         displayMessage: 'Invalid input - email'
--- processors/user/userProcessor.js
+++ processors/user/userProcessor.js
@@ -8,26 +8,36 @@ const User = require('../../models/user');
 const jwt = require('../../utils/jwt');
 const apiResponse = require('../../utils/apiResponse');
 
-let userLogin = async (req, res) => {
-    // let commonPW = await bcrypt.hash('123456', bcrypt.genSaltSync(parseInt(process.env.SALT_LENGTH)));
-    // let updateResult = await User.updateMany({}, { password: commonPW });
-    try {
-        let userResult = await User.findOne({ email: req.body.email });
-        if (userResult) {
-            let isPwCorrect = bcrypt.compareSync(req.body.password, userResult.password);
-            if (isPwCorrect) {
-                let returnResult = {};
-                returnResult.name = userResult.name;
-                returnResult.email = userResult.email;
-                
-                let token = jwt.createToken(returnResult);
-                returnResult.token = token;
-                apiResponse(req, res, 1001, returnResult);
-            } else {
-                apiResponse(req, res, 1001, 'wrong password');
+let validate = (req, res, method) => {
+    switch (method) {
+        case 'userLogin':
+            if (!validator.isEmail(req.body.email) || validator.isEmpty(req.body.password)) {
+                return apiResponse(req, res, 1006, []);
             }
-        } else {
-            apiResponse(req, res, 1003, {});
+            break;
+    }
+};
+
+let userLogin = async (req, res) => {
+    try {
+        if (validate(req, res, 'userLogin')) {
+            let userResult = await User.findOne({ email: req.body.email });
+            if (userResult) {
+                let isPwCorrect = bcrypt.compareSync(req.body.password, userResult.password);
+                if (isPwCorrect) {
+                    let returnResult = {};
+                    returnResult.name = userResult.name;
+                    returnResult.email = userResult.email;
+                    
+                    let token = jwt.createToken(returnResult);
+                    returnResult.token = token;
+                    apiResponse(req, res, 1001, returnResult);
+                } else {
+                    apiResponse(req, res, 1001, 'wrong password');
+                }
+            } else {
+                apiResponse(req, res, 1003, {});
+            }
         }
     } catch (error) {
         apiResponse(req, res, 1009, error);
